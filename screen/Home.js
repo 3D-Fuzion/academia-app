5a18b74a0d8512aa9f610f34d0e97307daa2a540
Guilherme-Vianna <mataveli91@gmail.com>
2023-09-01 09:21:20 -0300

    feat: Modal com botoes para navegacao adicionado

--- screen/Home.js
+++ screen/Home.js
@@ -1,10 +1,11 @@
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Text, View, Modal} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
 import {useState} from 'react';
+import {TouchableOpacity} from 'react-native';
+import {transform} from 'typescript';
 
 export default function Home() {
-  const [menuModal, setMenuModal] = useState(true);
+  const [menuModal, setMenuModal] = useState(false);
 
   function SwitchModal() {
     if (menuModal === false) {
@@ -24,7 +25,7 @@ export default function Home() {
         justifyContent: 'center',
       }}>
       <Modal
-        animationType="fade"
+        animationType="slide"
         transparent={true}
         visible={menuModal}
         onRequestClose={() => {
@@ -32,57 +33,101 @@ export default function Home() {
         }}>
         <View
           style={{
-            flex: 1,
+            width: 300,
+            height: 450,
+            transform: [{translateY: 180}],
+            alignSelf: 'center',
+            backgroundColor: '#187B63',
+            borderRadius: 10,
+            padding: 10,
+            gap: 1,
           }}>
-          <Text>Modal</Text>
-        </View>
-      </Modal>
-      <SafeAreaView
-        style={{
-          flexDirection: 'row',
-          width: '100%',
-          height: '8%',
-          backgroundColor: 'gray',
-        }}>
-        <SafeAreaView
-          style={{
-            flexDirection: 'row',
-            flex: 3,
-            backgroundColor: 'blue',
-          }}>
-          <SafeAreaView
-            style={{
-              flex: 1,
-              backgroundColor: 'yellow',
-              borderWidth: 1,
-              borderRadius: 100,
-            }}></SafeAreaView>
-          <SafeAreaView
-            style={{
-              flex: 3,
-              backgroundColor: 'orange',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+          <TouchableOpacity style={{flex: 1}}>
+            <Text style={{textAlign: 'center', fontSize: 40, color: 'white'}}>
+              Perfil
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{flex: 1}}>
+            <Text style={{textAlign: 'center', fontSize: 40, color: 'white'}}>
+              CheckIn
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{flex: 1}}>
+            <Text style={{textAlign: 'center', fontSize: 40, color: 'white'}}>
+              Treino do Dia
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{flex: 1}}>
+            <Text style={{textAlign: 'center', fontSize: 40, color: 'white'}}>
+              Calendario
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}>
             <Text
               style={{
-                fontSize: 40,
-                fontWeight: 'bold',
+                textAlign: 'center',
+                fontSize: 30,
+                color: 'white',
               }}>
-              Pessoa
+              Recorde Pessoal
             </Text>
-          </SafeAreaView>
-        </SafeAreaView>
-        <SafeAreaView
-          style={{
-            flex: 1,
-            backgroundColor: 'green',
-          }}>
+          </TouchableOpacity>
+          <TouchableOpacity style={{flex: 1}}>
+            <Text style={{textAlign: 'center', fontSize: 40, color: 'white'}}>
+              Atletas
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{flex: 1}}>
+            <Text style={{textAlign: 'center', fontSize: 40, color: 'white'}}>
+              Pagamento
+            </Text>
+          </TouchableOpacity>
           <TouchableOpacity
-            onPress={SwitchModal}
-            style={{width: '100%', height: '100%'}}></TouchableOpacity>
-        </SafeAreaView>
-      </SafeAreaView>
+            onPress={() => setMenuModal(false)}
+            style={{flex: 1}}>
+            <Text style={{textAlign: 'center', fontSize: 40, color: 'white'}}>
+              Sair
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#D8D8D8',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text style={{textAlign: 'center', flex: 1, fontSize: 24}}>
+            Icone
+          </Text>
+          <Text style={{textAlign: 'center', flex: 1, fontSize: 24}}>Nome</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              flex: 1,
+              height: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#A8A8A8',
+            }}>
+            <Text style={{textAlign: 'center'}}>Icone1</Text>
+            <TouchableOpacity
+              style={{backgroundColor: 'red'}}
+              onPressIn={SwitchModal}>
+              <Text style={{textAlign: 'center'}}>Icone2</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View
+        style={{flex: 9, height: '100%', backgroundColor: '#ECECEC'}}></View>
     </SafeAreaView>
   );
 }
