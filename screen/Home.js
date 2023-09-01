916b960943f59ad74699291d546143237b5e2eee
Guilherme Vianna <mataveli91@gmail.com>
2023-08-31 21:09:21 -0300

    feat: Base pra modal adicionado

--- screen/Home.js
+++ screen/Home.js
@@ -1,14 +1,42 @@
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text, View} from 'react-native';
+import {Text, View, Modal} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useState} from 'react';
 
 export default function Home() {
+  const [menuModal, setMenuModal] = useState(true);
+
+  function SwitchModal() {
+    if (menuModal === false) {
+      setMenuModal(true);
+      console.log('Modal aberto');
+    } else {
+      setMenuModal(false);
+      console.log('Modal fechado');
+    }
+  }
+
   return (
     <SafeAreaView
       style={{
-        width: '100%',
-        height: '100%',
+        flex: 1,
         alignItems: 'center',
+        justifyContent: 'center',
       }}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={menuModal}
+        onRequestClose={() => {
+          setMenuModal(!menuModal);
+        }}>
+        <View
+          style={{
+            flex: 1,
+          }}>
+          <Text>Modal</Text>
+        </View>
+      </Modal>
       <SafeAreaView
         style={{
           flexDirection: 'row',
@@ -48,8 +76,12 @@ export default function Home() {
         <SafeAreaView
           style={{
             flex: 1,
-            backgroundColor: 'red',
-          }}></SafeAreaView>
+            backgroundColor: 'green',
+          }}>
+          <TouchableOpacity
+            onPress={SwitchModal}
+            style={{width: '100%', height: '100%'}}></TouchableOpacity>
+        </SafeAreaView>
       </SafeAreaView>
     </SafeAreaView>
   );
