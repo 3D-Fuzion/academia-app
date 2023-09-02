8b64f45d2c775ac2fc810f8ae5bf46e1ce9f97b0
Guilherme Vianna <mataveli91@gmail.com>
2023-09-02 18:33:24 -0300

    App.js

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Register from './screen/Register';
 import Login from './screen/Login';
 import Profile from './screen/Profile';
+import CheckIn from './screen/CheckIn';
 
 const Stack = createStackNavigator();
 
@@ -14,12 +15,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Profile"
+        initialRouteName="CheckIn"
         screenOptions={{
           headerShown: false,
         }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="CheckIn" component={CheckIn} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Intro" component={Intro} />
