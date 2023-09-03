fa4a01d2cb780e878ee4cc62e318fee65be05ec0
Guilherme Vianna <mataveli91@gmail.com>
2023-09-03 10:21:44 -0300

    feat: Tela de feed adicionado

--- components/TopBar.js
+++ components/TopBar.js
@@ -6,6 +6,7 @@ import Menu from '../assets/menu';
 import TreinoDoDia from '../assets/screen_titles/treinododia';
 import Atletas from '../assets/screen_titles/atletas';
 import Payment from '../assets/screen_titles/fatura';
+import Feed from '../assets/screen_titles/logo';
 export default function TopBar({method, variable, screenTitle}) {
   let title;
   function TriggerMethod() {
@@ -17,6 +18,8 @@ export default function TopBar({method, variable, screenTitle}) {
     title = <Atletas width={80} height={80} />;
   } else if (screenTitle == 'pagamento') {
     title = <Payment width={80} height={80} />;
+  } else if (screenTitle == 'feed') {
+    title = <Feed width={150} height={150} />;
   }
   return (
     <>
@@ -25,13 +28,19 @@ export default function TopBar({method, variable, screenTitle}) {
           flex: 2,
           width: '100%',
           alignItems: 'center',
+          backgroundColor: 'white',
         }}>
-        <SafeAreaView style={{alignSelf: 'flex-start', marginLeft: 30}}>
+        <SafeAreaView
+          style={{
+            alignSelf: 'flex-start',
+            marginLeft: 30,
+          }}>
           {title}
         </SafeAreaView>
       </SafeAreaView>
       <SafeAreaView
         style={{
+          backgroundColor: 'white',
           flex: 1,
           flexDirection: 'row',
           width: '100%',
@@ -40,25 +49,25 @@ export default function TopBar({method, variable, screenTitle}) {
           justifyContent: 'center',
           gap: 20,
         }}>
-        <Sino width={35} height={35} />
+        <Sino width={32} height={32} />
         <SafeAreaView
           style={{width: 50, justifyContent: 'center', alignItems: 'center'}}>
           {variable ? (
             <TouchableOpacity onPress={TriggerMethod}>
               <SafeAreaView
                 style={{
-                  borderWidth: 8,
+                  borderWidth: 7,
                   borderColor: '#1A4239',
                   borderRadius: 10,
                   backgroundColor: '#1A4239',
                 }}>
-                <Menu width={35} height={35} />
+                <Menu width={24} height={22} />
               </SafeAreaView>
             </TouchableOpacity>
           ) : (
             <TouchableOpacity onPress={TriggerMethod}>
               <SafeAreaView>
-                <Menu width={35} height={35} />
+                <Menu width={24} height={22} />
               </SafeAreaView>
             </TouchableOpacity>
           )}
