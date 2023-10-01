5835aadc95b1e6e2b10fca046cfd8fec3b47c60d
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-10-01 19:15:53 -0300

    change init screen

--- App.jsx
+++ App.jsx
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Feed"
+        initialRouteName="Profile"
         screenOptions={{
           headerShown: false,
         }}>
