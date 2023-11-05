898ae70837712278c2a3d68744f4fffbb4f069ae
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-11-05 20:13:24 -0300

    add navigation option to command bar

--- components/CommandBar.jsx
+++ components/CommandBar.jsx
@@ -1,9 +1,9 @@
-import {TouchableOpacity} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Home from '../assets/command_bar/home';
 import Treino from '../assets/command_bar/treino';
 import Post from '../assets/command_bar/post';
-export default function CommandBar() {
+export default function CommandBar({ goto }) {
   return (
     <SafeAreaView
       style={{
@@ -16,10 +16,10 @@ export default function CommandBar() {
         flexDirection: 'row',
         backgroundColor: '#187B63',
       }}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => goto("Feed")}>
         <Home width={30} height={30} />
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => goto("Record")}>
         <Treino width={45} height={45} />
       </TouchableOpacity>
       <TouchableOpacity>
