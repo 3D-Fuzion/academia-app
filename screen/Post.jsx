1ed664a1093fa23e4dd99f85f8ee0dc5192153d2
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-11-21 21:30:53 -0300

    selection of image added

--- screen/Post.jsx
+++ screen/Post.jsx
@@ -1,7 +1,26 @@
 import { TextInput, TouchableOpacity, Text, KeyboardAvoidingView } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import DocumentPicker from 'react-native-document-picker';
+import { useState } from "react";
 
 export default function Post({ navigation }) {
+  const [image, setImage] = useState("")
+
+
+  async function SelectFile() {
+    try {
+      const result = await DocumentPicker.pickSingle({
+        presentationStyle: "fullScreen"
+      });
+      setImage([result])
+      console.log(image);
+    } catch (error) {
+      handleError(e)
+    }
+  }
+
+  //TODO UPDATE IMAGE LOGIC
+
   return (
     <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
       <SafeAreaView style={{ flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
@@ -16,7 +35,7 @@ export default function Post({ navigation }) {
         <SafeAreaView style={{ flex: 1 }}>
           <SafeAreaView style={{ flex: 5 }}>
             <SafeAreaView style={{ flex: 1, margin: 20, elevation: 10, borderRadius: 10, backgroundColor: "#ECECEC", flexDirection: "row" }}>
-              <TouchableOpacity style={{ margin: 20, alignSelf: "flex-end", marginLeft: "auto", height: 39, width: 79, backgroundColor: "green", justifyContent: "center", alignItems: "center", borderRadius: 40 }}>
+              <TouchableOpacity onPress={() => SelectFile()} style={{ margin: 20, alignSelf: "flex-end", marginLeft: "auto", height: 39, width: 79, backgroundColor: "green", justifyContent: "center", alignItems: "center", borderRadius: 40 }}>
                 <Text style={{ color: "white" }}>Adicionar</Text>
               </TouchableOpacity>
             </SafeAreaView>
